0c87c755a0d603a5dae1700cd7970764002b1fd7
bamaswampmonster <bamaswampmonster@bamaswampmonsters-MacBook-Pro.local>
2015-09-16 11:56:14 -0500

    learning to run tests

--- testing.js
+++ testing.js
@@ -5,14 +5,42 @@
 
 function testLast(){
 	var fruits =['apple','orange','tomato','grape'];
-	console.log(_.last(fruits));
+	console.log("last = " + _.last(fruits));
 }
-
 testLast();
 
 function testFirst(){
 	var fruits =['apple','orange','tomato','grape'];
-	console.log(_.first(fruits));
+	console.log("first = " + _.first(fruits));
 }
-
 testFirst();
+
+
+
+//function testEach(){
+//	var fruits =['apple','orange','tomato','grape'];
+//	console.log("each = " + _.each(fruits));
+//}
+//testEach();
+
+
+
+function testFilter(){
+	var fruits =['apple','orange','tomato','grape'];
+		console.log(fruits);
+		console.log("filter = " + _.filter(fruits));
+	//if(fruits = 'tomato'){
+		//return true;
+	//}else{
+		//return false;
+	//}
+}
+testFilter();
+
+
+function testReject(){
+	var fruits = ['apple','orange','tomato','grape'];
+		console.log(fruits);
+		console.log("reject = " + _.reject(fruits));
+}
+testReject();
